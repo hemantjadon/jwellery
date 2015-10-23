d8f199c1c7d79b949c17c2f6f6b9153c2b83f331
tanvi-surana <tanvisurana20@gmail.com>
2015-10-23 15:32:58 +0530

    random

---
+++ roughViews/urls.py
@@ -0,0 +1,32 @@
+# from django.conf.urls import url,include
+# from roughViews.views import *
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+# 				url(r'^$',initialPage,name='initial_page'),
+# 				url(r'^jwellery/$',jwelleryPage,name='jwellery_page'),
+# 				url(r'^jwellery/product',productPage,name='product'),
+# 				url(r'^solitares/$',solitaresPage,name='solitares_page'),
+# 				url(r'^goldCoins/$',goldCoinsPage,name='goldCoins_page'),
+# 				url(r'^collections/$',collectionsPage,name='collections_page'),
+# 				url(r'^gifts/$',giftsPage,name='gifts_page'),
+# 			]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.conf.urls import url,patterns,include
+from roughViews.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = patterns('',
+				url(r'^$',initialPage,name='initial_page'),
+				url(r'^jwellery/$',jwelleryPage,name='jwellery_page'),
+				url(r'^jwellery/product',productPage,name='product'),
+				url(r'^solitares/$',solitaresPage,name='solitares_page'),
+				url(r'^goldCoins/$',goldCoinsPage,name='goldCoins_page'),
+				url(r'^collections/$',collectionsPage,name='collections_page'),
+				url(r'^gifts/$',giftsPage,name='gifts_page'),
+				url(r'^fb/$',fbPage,name='fb_page'),
+				url(r'^product_detail/$',product_detail_page,name='product_detail'),
+
+			)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ roughViews/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,redirect
+from Products.models import *
+from roughViews.forms import filterForm
+from django.http import HttpResponse
+from django.core import serializers
+from django.apps import apps
+
+# Create your views here.
+def initialPage(request):
+	return render(request,'initialPage/initialPageAccordian.html',{})
+
+def fbPage(request):
+    return render(request,'login-signup/login-signup.html',{})	
+
+# def fbPage2(request):
+#     return render(request,'login-signup2/login-signup2.html',{})	    
+
+def jwelleryPage(request):
+	return render(request,'jwelleryPage/jwelleryPage.html',{"page":"jwellery"})
+
+def solitaresPage(request):
+	return render(request,'jwelleryPage/jwelleryPage.html',{"page":"solitares"})
+
+def goldCoinsPage(request):
+	return render(request,'jwelleryPage/jwelleryPage.html',{"page":"goldCoins"})
+
+def collectionsPage(request):
+	return render(request,'jwelleryPage/jwelleryPage.html',{"page":"collections"})
+
+def giftsPage(request):
+	return render(request,'jwelleryPage/jwelleryPage.html',{"page":"gifts"})
+
+def productPage(request):
+	identifier=request.GET.getlist('identifier')
+	products=[]
+	if ((len(identifier) is not 0) and (identifier[0] is not '')):
+		app=apps.get_app_config('Products')
+		models=app.get_model(identifier[0])
+		print(models)
+		products+=models.objects.all()
+
+	return render(request,'jwelleryProductPage/jewlleryProductPage.html',{"page":"jwellery","filterForm":filterForm,"products":products})
+ 
